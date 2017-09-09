fa5aa91f5667ac1e8a08245cadf5399b362a99ad
Dan Rodrigues <Danieljosephrodrigues@gmail.com>
2017-09-08 17:56:36 -0700

    Created Add job functionality in the JobController class. Validation happens through the Model and is presented in the View if an error occurs.

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,14 +1,13 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.Job;
-import org.launchcode.models.JobField;
-import org.launchcode.models.JobFieldType;
+import org.launchcode.models.*;
 import org.launchcode.models.data.JobFieldData;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,13 +39,24 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @ModelAttribute @Valid JobForm jobForm, Errors errors) {
+
+        if (errors.hasErrors()){
+            return "new-job";
+        }
+        Employer aEmployer = jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location aLocation = jobData.getLocations().findById(jobForm.getLocationId());
+        PositionType aPositionType = jobData.getPositionTypes().findById(jobForm.getPositionTypesId());
+        CoreCompetency aCoreCompetency = jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId());
+
+        Job job = new Job(jobForm.getName(),aEmployer,aLocation,aPositionType,aCoreCompetency);
+        jobData.add(job);
 
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        return "redirect:?id="+job.getId();
 
     }
 }
